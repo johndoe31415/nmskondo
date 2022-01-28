a4dbab60c87425357f74da9a39a6ad87fdd26de5
Johannes Bauer <joe@johannes-bauer.com>
2022-01-28 17:44:03 +0100

    Added missing executable
    The actual executable was missing for 18 months... now it's included.
    Fixes #1.

---
+++ nmskondo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#	nmskondo - Tools for the game "No Man's Sky"
+#	Copyright (C) 2020-2020 Johannes Bauer
+#
+#	This file is part of nmskondo.
+#
+#	nmskondo is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; this program is ONLY licensed under
+#	version 3 of the License, later versions are explicitly excluded.
+#
+#	nmskondo is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with nmskondo; if not, write to the Free Software
+#	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#	Johannes Bauer <JohannesBauer@gmx.de>
+
+import nms.__main__
